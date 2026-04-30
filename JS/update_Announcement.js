be489a6c3707c32b43f333afacb671da03e24daa
zzzer0 <noodlebrain91@gmail.com>
2026-04-29 18:37:06 -0700

    Update update_Announcement.js

--- JS/update_Announcement.js
+++ JS/update_Announcement.js
@@ -1,19 +1,40 @@
 // Update News
 function text()
 {
-    const text = "Hello, I love reading and writing.";
+    const texts = 
+    [
+        "Hello. I have come with updates. \nI have completed my Ascii_Scan program, it will be available for download on another new page. ", 
+        "Check out this sketch I drew: \n", 
+        "Here is some concept art i've drawn for my game"
+    ];
+    const text_id = ["update", "update_2", "update_3"];
     let index = 0;
+    let char = 0;
 
-    function typingEffect()
+    function typing()
     {
-        if (index < text.length)
+        if (index < texts.length)
         {
-            document.getElementById("typing").textContent += text[index];
-            index++;
-            setTimeout(typingEffect, 30); // Speed
+            const currentText = texts[index];
+            const textID = text_id[index] || "update";
+            const text = document.getElementById(textID);
+
+            if (char < currentText.length)
+            {
+                text.textContent += currentText[char];
+                char++;
+                setTimeout(typing, 30);
+            }
+            else
+            {
+                index++;
+                char = 0;
+                setTimeout(typing, 500);
+            }
         }
     }
-    typingEffect();
+    typing();
+
 }
 
 function messageBoard()
